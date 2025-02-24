c3b787f368ef0db4faf0746dfa0d67ac3185e7c1
Yakov Leontiev <leo-yakov@yandex.ru>
2025-02-24 18:19:21 +0300

    Routing fixes

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, "dist"),
     filename: "index.js",
+    publicPath: "/",
   },
   module: {
     rules: [
@@ -41,5 +42,6 @@ module.exports = {
   devServer: {
     watchFiles: path.join(__dirname, "src"),
     port: 8002,
+    historyApiFallback: true,
   },
 };
